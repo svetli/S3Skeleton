2f76a82351f93ee7d43d97c4f1523e4d1588a434
abr4xas <me@abr4xas.org>
2016-03-19 22:44:38 +0000

    updated

--- app/Common/User/UserPermission.php
+++ app/Common/User/UserPermission.php
@@ -9,8 +9,11 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class UserPermission extends Eloquent
 {
+    /**
+    * @var string $table 				Name of the table that this class models.
+    * @var array  $fillable 			Array of the columns this model can modify.
+    */
     protected $table = 'users_permissions';
-
     protected $fillable = [
         'is_admin',
         'is_mod'
