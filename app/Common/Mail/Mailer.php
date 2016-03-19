7e7052666d0f1baf2b8522a7adf5718527df80ba
abr4xas <me@abr4xas.org>
2016-03-19 22:49:19 +0000

    updated

--- app/Common/Mail/Mailer.php
+++ app/Common/Mail/Mailer.php
@@ -4,50 +4,51 @@
 */
 
 namespace App\Common\Mail;
+use \App\Common\Mail\Message;
 
-class Mailer 
+class Mailer
 {
-	/**
-	* @var \Slim\Views\Twig $view 		The container's view dependency.
-	* @var PHPMailer 		$mailer 	A PHPMailer instance with our mail config.
-	*/
-	protected $view;
-	protected $mailer;
-	
-	/**
-	* Constructor
-	*
-	* @param \Slim\Views\Twig 	$view 	The container's view dependency.
-	* @param PHPMailer 			$mailer A PHPMailer instance with our mail config.
-	*/
-	public function __construct($view, $mailer)
-	{
-		$this->view = $view;
-		$this->mailer = $mailer;
-	} //End constructor
+    /**
+    * @var \Slim\Views\Twig $view 		The container's view dependency.
+    * @var PHPMailer 		$mailer 	A PHPMailer instance with our mail config.
+    */
+    protected $view;
+    protected $mailer;
 
-	/**
-	* Grabs the appropriate template, creates a message body, and executes the
-	* callback function before sending the email.
-	*
-	* @param string 	$template 	A view template for the email to be sent.
-	* @param array  	$data 		Array of data to pass to the view.
-	* @param function  	$callback 	The callback function that is executed after the message is created.
-	*								Usually used to add details to the message such as the receiver & subject.
-	* @return null
-	*/
-	public function send($template, $data, $callback)
-	{
-		//	First create a new instance of our Message class passing our mailer object.
-		$message = new \App\Common\Mail\Message($this->mailer);
+    /**
+    * Constructor
+    *
+    * @param \Slim\Views\Twig 	$view 	The container's view dependency.
+    * @param PHPMailer 			$mailer A PHPMailer instance with our mail config.
+    */
+    public function __construct($view, $mailer)
+    {
+        $this->view = $view;
+        $this->mailer = $mailer;
+    } //End constructor
 
-		//	Now, send the template with the appropriate data to the message body.
-		$message->body($this->view->fetch($template, $data));
+    /**
+    * Grabs the appropriate template, creates a message body, and executes the
+    * callback function before sending the email.
+    *
+    * @param string 	$template 	A view template for the email to be sent.
+    * @param array  	$data 		Array of data to pass to the view.
+    * @param function  	$callback 	The callback function that is executed after the message is created.
+    *								Usually used to add details to the message such as the receiver & subject.
+    * @return null
+    */
+    public function send($template, $data, $callback)
+    {
+        //	First create a new instance of our Message class passing our mailer object.
+        $message = new Message($this->mailer);
 
-		//	Execute our callback function.
-		call_user_func($callback, $message);
+        //	Now, send the template with the appropriate data to the message body.
+        $message->body($this->view->fetch($template, $data));
 
-		//	And send the message.
-		$this->mailer->send();
-	} //End send
+        //	Execute our callback function.
+        call_user_func($callback, $message);
+
+        //	And send the message.
+        $this->mailer->send();
+    } //End send
 } //End class mailer
