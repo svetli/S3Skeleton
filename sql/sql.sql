ef4042115ff7d076725d49b933b22c1fc21d5913
abr4xas <me@abr4xas.org>
2016-03-23 02:10:24 +0000

    new file

---
+++ sql/sql.sql
@@ -0,0 +1,78 @@
+-- Adminer 4.2.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1,	'Test',	'2016-03-09 18:32:49',	'2016-03-09 18:32:49');
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+    `excerpt` tinytext COLLATE utf8_unicode_ci NOT NULL,
+    `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+    `body` longtext COLLATE utf8_unicode_ci NOT NULL,
+    `status` tinyint(1) NOT NULL,
+    `user_id` int(11) DEFAULT NULL,
+    `category_id` int(11) DEFAULT NULL,
+    `seo_url` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`),
+    KEY `user_post_idx` (`user_id`),
+    CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `posts` (`id`, `title`, `excerpt`, `image`, `body`, `status`, `user_id`, `category_id`, `seo_url`, `created_at`, `updated_at`) VALUES
+(1,	'Lorem ipsum dolor sit amet, con ',	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt enim enim, quis consequat metus lobortis non. Ut at nibh est. Proin condimentum erat vel lacus imperdiet feugiat.',	'56e5887690f18.jpg',	'<p><span style=\"line-height: 1.5;\">My money\'s in that office, right? If she start giving me some bullshit about it ain\'t there, and we got to go someplace else and get it, I\'m gonna shoot you in the head then and there. Then I\'m gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I\'m talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Now that we know who you are, I know who I am. I\'m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain\'s going to be? He\'s the exact opposite of the hero. And most times they\'re friends, like you and me! I should\'ve known way back when... You know why, David? Because of the kids. They called me Mr Glass.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother\'s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Your bones don\'t break, mine do. That\'s clear. Your cells react to bacteria and viruses differently than mine. You don\'t get sick, I do. That\'s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We\'re on the same curve, just on opposite ends.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that\'s what you see at a toy store. And you must think you\'re in a toy store, because you\'re here shopping for an infant named Jeb.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">My money\'s in that office, right? If she start giving me some bullshit about it ain\'t there, and we got to go someplace else and get it, I\'m gonna shoot you in the head then and there. Then I\'m gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I\'m talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Your bones don\'t break, mine do. That\'s clear. Your cells react to bacteria and viruses differently than mine. You don\'t get sick, I do. That\'s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We\'re on the same curve, just on opposite ends.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Look, just because I don\'t be givin\' no man a foot massage don\'t make it right for Marsellus to throw Antwone into a glass motherfuckin\' house, fuckin\' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, \'cause I\'ll kill the motherfucker, know what I\'m sayin\'?</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that\'s what you see at a toy store. And you must think you\'re in a toy store, because you\'re here shopping for an infant named Jeb.</span></p><p><span style=\"line-height: 1.5;\"><br></span><span style=\"line-height: 1.5;\">Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I\'m in a transitional period so I don\'t wanna kill you, I wanna help you. But I can\'t give you this case, it don\'t belong to me. Besides, I\'ve already been through too much shit this morning over this case to hand it over to your dumb ass.</span></p>',	1,	22,	1,	'lorem-ipsum-dolor-sit-amet-con',	'2016-03-13 16:08:29',	'2016-03-13 16:08:29');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+    `first_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `last_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+    `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+    `about_me` tinytext COLLATE utf8_unicode_ci NOT NULL,
+    `is_active` tinyint(1) DEFAULT NULL,
+    `active_hash` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `recover_hash` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `remember_identifier` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `position` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `about_me`, `is_active`, `active_hash`, `recover_hash`, `remember_identifier`, `remember_token`, `position`, `created_at`, `updated_at`) VALUES
+(1,	'admin',	'Lorem',	'Ipsum',	'me@domain.tld',	'$2y$10$utixb/2vB.sxQvv/k8hrIOANyibQm/v5SYSYkZjSUNzT9ZUYOEh0q',	'Lispum',	1,	NULL,	NULL,	NULL,	NULL,	'Lipsum',	'2016-03-19 21:19:04',	'2016-03-19 21:19:04');
+
+DROP TABLE IF EXISTS `users_permissions`;
+CREATE TABLE `users_permissions` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) unsigned NOT NULL,
+    `is_admin` tinyint(1) unsigned NOT NULL,
+    `is_mod` tinyint(1) unsigned NOT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users_permissions` (`id`, `user_id`, `is_admin`, `is_mod`, `created_at`, `updated_at`) VALUES
+(1,	1,	1,	1,	'2016-02-16 15:42:16',	'2016-02-16 15:42:16');
+
+-- 2016-03-23 02:05:07
