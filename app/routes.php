94067e604a5c316c2eaa67023abe477100e9c279
abr4xas <me@abr4xas.org>
2016-03-28 21:05:08 +0000

    updated

--- app/routes.php
+++ app/routes.php
@@ -11,17 +11,17 @@ if (!defined('IN_PROJECT'))
 require(GLOBAL_ROOT_PATH . '/app/routes/auth/auth_routes' . PHP_EXT);
 require(GLOBAL_ROOT_PATH . '/app/routes/admin/admin_routes' . PHP_EXT);
 
-$app->get('/', function ($request, $response)  use ($container) {
+$app->get('/', function ($request, $response) {
 
     // Get flash messages from previous request
     $messages = $this->flash->getMessages();
 
-    $PostAll = $container->post
+    $PostAll = $this->post
         ->where('status', 1)
         ->orderBy('id', 'desc')
         ->get();
 
-    $posted = $container->post
+    $posted = $this->post
         ->where('status', 1)
         ->orderBy('id', 'desc')
         ->take(5)
@@ -35,14 +35,14 @@ $app->get('/', function ($request, $response)  use ($container) {
 
 })->setName('home');
 
-$app->get('/post/[{id:[0-9]+}/[{name:[A-Za-z-]+}]]', function ($request, $response, $args)  use ($container) {
+$app->get('/post/[{id:[0-9]+}/[{name:[A-Za-z-]+}]]', function ($request, $response, $args) {
 
     //	Feel free to change it. :)
 
     $id = $args['id'];
     $name = $args['name'];
 
-    $posted = $container->post
+    $posted = $this->post
         ->where('id','=', "$id")
         ->where('seo_url', '=',"$name")
         ->first();
